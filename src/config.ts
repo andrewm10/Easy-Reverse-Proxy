86de59912f29e24cf9f1eaee167075cf463779d2
andrewm10 <33991122+andrewm10@users.noreply.github.com>
2024-12-03 17:29:23 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -4,4 +4,11 @@ export let PORT = 370; // 端口
 
 export let PROXY_CODE_SECRET = 'Easy-Reverse-Proxy'; // 代理码密钥
 // 代理
-export let PROXYS: Proxy[] = [];
+export let PROXYS: Proxy[] = [
+    {
+        domain: "gh.20140213.xyz",
+        url: "https://github.com/",
+        enable: true,
+        template: Github
+    }
+];
